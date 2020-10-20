6021b6964e20e2c2a99309643050028ef0d6bff9
SekineKazanari <56060858+SekineKazanari@users.noreply.github.com>
2020-10-20 12:58:39 -0600

    validación de rutas

---
+++ resources/views/suma.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Suma</title>
+	<style type="text/css">
+		h1{
+			text-align: center;
+		}
+		p{
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>
+		Suma
+	</h1>
+	<p>
+		El resultado es: <b> {{ $num1 + $num2}} </b>
+	</p>
+</body>
+</html>
+<!--Cntrl+Shift+P
+install packet control
+Laravel blade
+-->
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/usuarios', function () {
+    return view('welcome');
+});
+
+Route::post('/crear',function(){
+	echo "hola";
+});
+
+Route::put('/actualizar',function(){
+	echo "hola2";
+});
+
+Route::delete('/eliminar',function(){
+	echo "hola3";
+});
+
+Route::match(array('GET', 'POST'),'/usuarios',function(){
+	return view('welcome');
+});
+
+Route::any('/usuarios',function(){
+	return view('welcome');
+});
+
+//Parametros
+Route::get('/usuarios/{id}',function($id){
+	echo "hola".$id;
+});
+
+//Parametros opcionales
+Route::get('/usuarios/{id?}',function($id=0){
+	echo "hola".$id;
+});
+
+//Validación de rutas
+Route::get('/suma/{num1}/{num2}',function($num1,$num2){
+
+	echo "El resultado es: ".($num1 + $num2)."<br>";
+	echo "El resultado es: ".($num1 - $num2);
+
+})->where(array('num1'=>'[0-9]+', 'num2'=>'[0-9]+'));
+//->where('num1','[0-9]+')->where('num2','[0-9]+');
+*/
+
+Route::get('/suma/{num1}/{num2}','WebController@suma')->where(array('num1'=>'[0-9]+', 'num2'=>'[0-9]+'));
+//1.- App\Http\Controllers\WebController@suma
+//2.- Se modifico la linea 29 en app/Providers//RouteServiceProvider.php (se descomentó y se le quitó los / sobrantes)
