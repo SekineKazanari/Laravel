5846cbb3782e919b4a94224d53be846ec20a0db0
SekineKazanari <56060858+SekineKazanari@users.noreply.github.com>
2020-11-12 13:07:36 -0700

    crud completo

--- routes/web.php
+++ routes/web.php
@@ -79,4 +79,8 @@ Route::group(['middleware' => ['auth']],function(){
 	Route::get('/categories','CategoryController@index');
 
 	Route::post('/categories','CategoryController@store');
+
+	Route::put('/categories','CategoryController@update');
+
+	Route::delete('/categories/{category}','CategoryController@destroy');
 });
